b0fb9bc1113841f7e660cf0dfd004866b09a1fbb
Uwancha <sito2alemu@gmail.com>
2023-07-16 12:05:03 +0300

    build: Add contact component to the page -Create module for the component -Import module to index.js -Add event listeners to render the component -Add styles and design layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import renderHome from "./home";
 import renderMenu from "./menu";
 import { form } from "./reservation";
+import { contact } from "./contact";
 
 const app = document.createElement("div");
 app.classList.add("app");
@@ -35,4 +36,12 @@ book.addEventListener("click", () => {
     const app = document.querySelector(".app");
     app.innerHTML = "";
     app.insertAdjacentHTML('afterbegin', form);
+})
+
+const contactUs = document.querySelector("#Contact");
+
+contactUs.addEventListener("click", () => {
+    const app = document.querySelector(".app");
+    app.innerHTML = "";
+    app.insertAdjacentHTML('afterbegin', contact);
 })
